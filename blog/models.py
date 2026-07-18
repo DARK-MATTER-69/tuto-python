0949dda308a3815cfe4ddd31b2715d1e28cc8180
DARK-MATTER <brayannwans@gmail.com>
2026-07-19 00:09:41 +0100

    models foreignkey

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# Model de type ForeignKey (plusieurs a un)
+
+class Auteur(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    titre = models.CharField(max_length=150)
+    contenu = models.TextField()
+    date_publication = models.DateTimeField(auto_now_add=True)
+    auther = models.ForeignKey(
+        to= Auteur,
+        on_delete=models.CASCADE
+    )
+    def __str__(self):
+        return f"{self.name} - {self.Auteur}"
+
