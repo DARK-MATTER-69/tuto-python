306f700a12d0faf6bd9e1aa099aa849ce543eb37
DARK-MATTER <brayannwans@gmail.com>
2026-07-21 23:32:19 +0100

    creation des templates

--- blogApp/admin.py
+++ blogApp/admin.py
@@ -4,7 +4,7 @@ from blogApp.models import Article
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('titre', 'auteur', 'date_de_creation')
-    list_filter = ('date_de_creation')
+    list_filter = ('date_de_creation', )
     search_fields = ('titre','contenu')
     
 admin.site.register(Article, ArticleAdmin)
