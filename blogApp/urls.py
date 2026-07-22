98d0fcb9d0abe4db77921c14a64bad9cf45d3bf2
DARK-MATTER <brayannwans@gmail.com>
2026-07-22 10:26:07 +0100

    configuration du systeme d'authentification

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -3,5 +3,8 @@ from blogApp import views
 name_app = 'blogapp'
 
 urlpatterns = [
-    path('',views.accueil_view)
+    path('',views.accueil_view, name='accueil'),
+    path('connexion/',views.connect_view, name='connexion'),
+    path('deconnexion/',views.deconnect_view, name='deconnexion'),
+    path('inscription/',views.inscription_view, name='inscription'),
 ]
--- blogApp/views.py
+++ blogApp/views.py
@@ -1,4 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login,logout
+
 
 def accueil_view(request):
     return render (request, 'accueil.html')
+
+def connect_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data = request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('blogapp:accueil')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'connexion.html', {'form':form})
+
+def inscription_view(request):
+    if request.method == 'POST':
+        forms = UserCreationForm(request.POST)
+        if forms.is_valid():
+            user = forms.save()
+            login(request, user)
+            return redirect('blogapp:accueil')
+    else:
+        forms = UserCreationForm()
+    return render(request, 'inscription.html', {'form': forms})
+
+def deconnect_view(request):
+    logout(request)
+    return redirect(request, 'blogapp:accueil.html')
